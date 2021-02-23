4dc4a4ef187a75692e0561d0681969d854b060b4
Vincent Bruzzese <vincentbruzzese.99@gmail.com>
2021-02-23 15:23:38 -0500

    working listing file, not with IR, needs to be modified

--- src/main/java/com/github/ConcordiaSOEN341/Driver/Driver.java
+++ src/main/java/com/github/ConcordiaSOEN341/Driver/Driver.java
@@ -30,47 +30,34 @@ public class Driver {
 
         try{
             File listingFile = new File ("testInherentMnemonics.lst");
-            if (listingFile.createNewFile()){
+            //if (listingFile.createNewFile()){
                 //System.out.println("File created");
-            }else{
+            //}else{
                 //System.out.println("File alreadyExists.");
-            }
+            //}
             FileWriter listingWriter = new FileWriter("testInherentMnemonics.lst");
-            listingWriter.write("Line Addr Code \t\t\tLabel \t\t  Mne \tOperand \t Comments\n");
+            listingWriter.write("Line Addr Code \t\t\tLabel \t\t  Mne \tOperand \t  Comments\n");
 
-            String[] TokenInLine = new String[5];
+            String[] tokenInLine = {"","",""};
             for(int i = 0; i < tokenList.size(); i ++) {
                 int lineNumber = tokenList.get(i).getLine();
 
-                if(tokenList.size() == i+1){
-                    break;
+                if (tokenList.get(i).getTokenType() == TokenType.MNEMONIC){
+                    tokenInLine[0] = tokenList.get(i).getTokenString();
+                }if (tokenList.get(i).getTokenType() == TokenType.LABEL){
+                    tokenInLine[1] = tokenList.get(i).getTokenString();
+                }if (tokenList.get(i).getTokenType() == TokenType.COMMENT){
+                    tokenInLine[2] = tokenList.get(i).getTokenString();
                 }
-
-                if(tokenList.get(i).getStartColumn() == 1){
-                    TokenInLine[0]= tokenList.get(i).getTokenString();
-                }else if (tokenList.get(i).getStartColumn() == 1){
-
-                }else if (tokenList.get(i).getStartColumn() == 1){
-
-                }else if (tokenList.get(i).getStartColumn() == 1){
-
-                }else if (tokenList.get(i).getStartColumn() == 1){
-
-                }
-
-                if(tokenList.get(i+1).getLine() != lineNumber){
-                    listingWriter.write(lineNumber +"\t "+Integer.toHexString(lineNumber)+" Code \t\t\tLabel \t\t  Mne \tOperand \t Comments");
-                    listingWriter.write("\n");
-                    continue;
+                if(tokenList.get(i).getTokenType()== TokenType.EOF||tokenList.get(i).getTokenType() == TokenType.EOL){
+                    String hexAddress = String.format("%04X", lineNumber-1);
+                    listingWriter.write(lineNumber +"\t "+hexAddress+" Code \t\t\t"+tokenInLine[1]+" \t\t\t  "+tokenInLine[0]+" \t\t \t\t\t "+tokenInLine[2]+"\n");
+                    tokenInLine[0]="";
+                    tokenInLine[1]="";
+                    tokenInLine[2]="";
                 }
             }
-
-
-
-
-
             listingWriter.close();
-
         }catch(IOException e){
             System.out.println("An error occurred");
             e.printStackTrace();
