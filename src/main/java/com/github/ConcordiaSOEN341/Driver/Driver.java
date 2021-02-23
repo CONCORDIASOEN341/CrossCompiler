d8b043eec0448e127e130b119b79b9a84aab416c
Vincent Bruzzese <vincentbruzzese.99@gmail.com>
2021-02-23 00:13:11 -0500

    work in progress

--- src/main/java/com/github/ConcordiaSOEN341/Driver/Driver.java
+++ src/main/java/com/github/ConcordiaSOEN341/Driver/Driver.java
@@ -36,7 +36,39 @@ public class Driver {
                 //System.out.println("File alreadyExists.");
             }
             FileWriter listingWriter = new FileWriter("testInherentMnemonics.lst");
-            listingWriter.write("Line Addr Code \t\t\tLabel \t\t  Mne \tOperand \t Comments");
+            listingWriter.write("Line Addr Code \t\t\tLabel \t\t  Mne \tOperand \t Comments\n");
+
+            String[] TokenInLine = new String[5];
+            for(int i = 0; i < tokenList.size(); i ++) {
+                int lineNumber = tokenList.get(i).getLine();
+
+                if(tokenList.size() == i+1){
+                    break;
+                }
+
+                if(tokenList.get(i).getStartColumn() == 1){
+                    TokenInLine[0]= tokenList.get(i).getTokenString();
+                }else if (tokenList.get(i).getStartColumn() == 1){
+
+                }else if (tokenList.get(i).getStartColumn() == 1){
+
+                }else if (tokenList.get(i).getStartColumn() == 1){
+
+                }else if (tokenList.get(i).getStartColumn() == 1){
+
+                }
+
+                if(tokenList.get(i+1).getLine() != lineNumber){
+                    listingWriter.write(lineNumber +"\t "+Integer.toHexString(lineNumber)+" Code \t\t\tLabel \t\t  Mne \tOperand \t Comments");
+                    listingWriter.write("\n");
+                    continue;
+                }
+            }
+
+
+
+
+
             listingWriter.close();
 
         }catch(IOException e){
