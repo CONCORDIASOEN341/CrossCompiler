b2f4003ad3a91faa7bacf0ad7657a50b22c3a86f
mimita <mimi_ta3000@hotmail.com>
2021-03-13 22:53:30 -0500

    SOEN-35: Implement Error class.

--- src/main/java/com/github/ConcordiaSOEN341/Error/Error.java
+++ src/main/java/com/github/ConcordiaSOEN341/Error/Error.java
@@ -1,4 +1,23 @@
 package com.github.ConcordiaSOEN341.Error;
 
+import com.github.ConcordiaSOEN341.Interfaces.ILineStatement;
+
 public class Error {
+    private ErrorType errorType;
+    private ILineStatement invalidLine;
+
+    public Error(ErrorType errorType, ILineStatement invalidLine) {
+        this.errorType = errorType;
+        this.invalidLine = invalidLine;
+    }
+
+    public ILineStatement getInvalidLine() {
+        return invalidLine;
+    }
+
+    // TODO: Change invalidLine.toString()
+    @Override
+    public String toString() {
+        return errorType.toString() + ": " + invalidLine.toString();
+    }
 }
