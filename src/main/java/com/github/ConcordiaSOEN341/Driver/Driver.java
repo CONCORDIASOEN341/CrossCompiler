cacfe67c5dd6f2a395e72aebcc25ad53e347716f
mimita <mimi_ta3000@hotmail.com>
2021-03-20 22:52:11 -0400

    Fix merge conflicts

--- src/main/java/com/github/ConcordiaSOEN341/Driver/Driver.java
+++ src/main/java/com/github/ConcordiaSOEN341/Driver/Driver.java
@@ -1,48 +1,22 @@
 package com.github.ConcordiaSOEN341.Driver;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import com.github.ConcordiaSOEN341.CommandHandle.CommandHandle;
 import com.github.ConcordiaSOEN341.CodeGen.CodeGen;
+import com.github.ConcordiaSOEN341.CommandHandle.CommandHandle;
+import com.github.ConcordiaSOEN341.Interfaces.ICodeGen;
+import com.github.ConcordiaSOEN341.Interfaces.IParser;
 import com.github.ConcordiaSOEN341.Lexer.Lexer;
-import com.github.ConcordiaSOEN341.Lexer.Token;
-import com.github.ConcordiaSOEN341.Lexer.TokenType;
-import com.github.ConcordiaSOEN341.Parser.LineStatement;
-import com.github.ConcordiaSOEN341.Reader.IReader;
-import com.github.ConcordiaSOEN341.Reader.Reader;
 import com.github.ConcordiaSOEN341.Parser.Parser;
-
-import java.util.ArrayList;
-
-import static java.lang.System.exit;
+import com.github.ConcordiaSOEN341.Reader.Reader;
 
 public class Driver {
     public static void main(String[] args) {
 
-
         CommandHandle commandHandle = new CommandHandle(args);
-        String file = commandHandle.getFile();
-        IReader reader = new Reader(file);
+        String fileName = commandHandle.getFile();
+        IParser parser = new Parser(new Lexer(new Reader(fileName)));
 
-        Lexer lexer = new Lexer(reader);
-        Parser parser = new Parser();
-        CodeGen cg = new CodeGen();
-
-        ArrayList<Token> tokenList = new ArrayList<>(); // Use this for listing file/parsing
-        Token t;
-        do{
-            t = lexer.getNextToken();
-            System.out.println(t);
-
-            tokenList.add(t);
-
-        }while(t.getTokenType() != TokenType.EOF);
-
-        ArrayList<LineStatement> ir = parser.generateIR(tokenList);
-
-        cg.generateListingFile(file,ir);
+        ICodeGen cg = new CodeGen();
+        cg.generateListingFile(fileName, parser.parse());
         commandHandle.delete();
     }
 
